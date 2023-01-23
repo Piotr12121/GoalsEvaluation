b1bf3f7b49bdea299a35740314204a178740c198
piotr.sz <p.szczerbetka@gmail.com>
2023-01-23 21:22:46 +0100

    basic migrations and models set up

---
+++ database/migrations/2023_01_23_201228_create_goals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->smallInteger('progress')->default(0)->nullable(false);
+            $table->unsignedBigInteger('employee_id')->nullable(false);
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+};
